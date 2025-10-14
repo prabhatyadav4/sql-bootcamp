94fd9ccd4afce0f382d2e99448483e5ae1f35fd0
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-10-14 23:44:46 +0530

    Initial Commit

---
+++ 10_NULL_Functions/SQLQuery03.sql
@@ -0,0 +1,8 @@
+-- Sort the customers from lowest to highest scores,
+-- with nulls appearing last.
+
+SELECT 
+	CustomerID,
+	Score
+FROM Sales.Customers
+ORDER BY CASE WHEN Score IS NULL THEN 1 ELSE 0 END, Score
