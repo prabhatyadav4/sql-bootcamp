aaa6642c425f0e0761267f50fa965fea09bbd548
prabhatyadav4 <osrprabhatyadav4@gmail.com>
2025-10-17 23:05:30 +0530

    Initial Commit

--- 11_Case_Statements/SQLQuery03.sql
+++ 11_Case_Statements/SQLQuery03.sql
@@ -9,7 +9,12 @@ SELECT
 		WHEN Country = 'Germany' THEN 'DE'
 		WHEN Country = 'USA' THEN 'US'
 		ELSE 'N/A'
-	END CountryAbbr
+	END CountryAbbr,
+	CASE Country
+		WHEN 'Germany' THEN 'DE'
+		WHEN 'USA' THEN 'US'
+		ELSE 'N/A'
+	END CountryAbbr2
 FROM Sales.Customers
 
 -- SELECT DISTINCT Country
